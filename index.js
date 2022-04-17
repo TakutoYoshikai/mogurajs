dcc8fa8a233c07798420fd5f6a5c75b34867f41e
lucky <takuto.yoshikai@gmail.com>
2022-04-17 14:51:18 +0900

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const Mogura = {
+  message: function() {
+    console.log("I have a hint.");
+  }
+}
+
+const hint1 = "https://github.com/TakutoYoshikai/phalanx";
+const hint2 = "Flag: N2Q2...";
+const hint3 = "length: 20";
+
+module.exports = Mogura;
